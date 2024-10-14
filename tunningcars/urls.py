150510e44b95adb244aed14abf651477b143b2c7
= <=>
2024-10-14 17:08:04 +0300

    usl

--- tunningcars/urls.py
+++ tunningcars/urls.py
@@ -32,4 +32,7 @@ urlpatterns += [
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
